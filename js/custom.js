924701e006da88f06a0633e63f49788f8978d2ff
Kevin Mann <kevinrmann@gmail.com>
2014-03-06 21:09:50 -0700

    Working on the datepicker

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
   $("select").selectpicker({style: 'btn-embossed btn-primary', menuStyle: 'dropdown-inverse'});
+
+  var $slider = $("#slider");
+  if ($slider.length > 0) {
+    $slider.slider({
+      min: 1,
+      max: 100,
+      value: 50,
+      orientation: "horizontal",
+      range: true
+    });
+  }
+
+  $('.datepicker').datepicker();
 });
